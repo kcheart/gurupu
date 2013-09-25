4faf6409824a4bd5d71677d85f0e55121b2c0b19
Kevin Chen <kcheart@gmail.com>
2013-09-25 14:41:55 +0800

    Cap deploy setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, "GURUPU"
+set :application, "gurupu"
 set :repository,  "git@github.com:kcheart/gurupu.git"
 
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
@@ -8,10 +8,15 @@ set :ssh_options, { :forward_agent => true }
 set :user, "railsapp"
 set :group, "railsapp"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "gurupu"                          # Your HTTP server, Apache/etc
+role :app, "gurupu"                          # This may be the same as your `Web` server
+role :db,  "gurupu", :primary => true # This is where Rails migrations will run
+role :db,  "gurupu"
+
+set :runner, "railsapp"
+set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+set :use_sudo, false
 
 set :deploy_to, "/home/#{user}/#{application}"
 
@@ -22,10 +27,10 @@ set :deploy_to, "/home/#{user}/#{application}"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
